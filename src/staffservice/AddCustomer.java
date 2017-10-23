6c227f7b1d0ef37025e4ee508c4a2701470b9d3c
snwijesuriya <snwijesuriya@CMDSNWIJESURIYA.Virtusa.com>
2017-10-23 16:33:25 +0530

    ffff

--- src/staffservice/AddCustomer.java
+++ src/staffservice/AddCustomer.java
@@ -5,31 +5,19 @@
  */
 package staffservice;
 
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-
 /**
  *
  * @author snwijesuriya
  */
-public class Payment extends javax.swing.JFrame {
+public class AddCustomer extends javax.swing.JFrame {
 
     /**
-     * Creates new form Payment
+     * Creates new form AddCustomer
      */
-    public Payment() {
+    public AddCustomer() {
         initComponents();
-        txtID.setEnabled(false);
-       
     }
-        public void chkEmpty() {
-  if(txtContactNo.getText()==null)
-  {
-      final JPanel panel = new JPanel();
-      System.out.println("sdadsadsadsa");
-  }
-        
-    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -46,23 +34,24 @@ public class Payment extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        txtContactNo = new javax.swing.JTextField();
-        txtAmount = new javax.swing.JTextField();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        txtDate = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
         jLabel6 = new javax.swing.JLabel();
+        txtContactNo1 = new javax.swing.JTextField();
+        txtContactNo2 = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        txtContactNo3 = new javax.swing.JTextField();
+        jLabel8 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setLocation(new java.awt.Point(0, 0));
         setLocationByPlatform(true);
         setMaximumSize(new java.awt.Dimension(700, 600));
         setMinimumSize(new java.awt.Dimension(700, 600));
-        getContentPane().setLayout(null);
+        setPreferredSize(new java.awt.Dimension(400, 300));
 
         jPanel1.setBackground(new java.awt.Color(153, 153, 153));
         jPanel1.setLayout(null);
@@ -85,7 +74,7 @@ public class Payment extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
         jLabel1.setText("Contact No   ");
         jPanel1.add(jLabel1);
-        jLabel1.setBounds(90, 230, 100, 16);
+        jLabel1.setBounds(90, 232, 100, 16);
 
         jLabel2.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
         jLabel2.setText("ID                 ");
@@ -97,40 +86,20 @@ public class Payment extends javax.swing.JFrame {
         jPanel1.add(jLabel3);
         jLabel3.setBounds(90, 160, 170, 16);
 
-        txtContactNo.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtContactNoActionPerformed(evt);
-            }
-        });
-        jPanel1.add(txtContactNo);
-        txtContactNo.setBounds(220, 230, 270, 25);
-
-        txtAmount.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtAmountActionPerformed(evt);
-            }
-        });
-        jPanel1.add(txtAmount);
-        txtAmount.setBounds(220, 290, 270, 25);
-
         jLabel4.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
-        jLabel4.setText("Date          ");
+        jLabel4.setText("Destination         ");
         jPanel1.add(jLabel4);
-        jLabel4.setBounds(90, 344, 100, 16);
+        jLabel4.setBounds(90, 344, 120, 16);
 
         jLabel5.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
-        jLabel5.setText("Amount        ");
+        jLabel5.setText("Starting ");
         jPanel1.add(jLabel5);
         jLabel5.setBounds(90, 294, 100, 16);
-
-        txtDate.setDateFormatString("yyyy-MM-dd");
-        jPanel1.add(txtDate);
-        txtDate.setBounds(220, 340, 270, 25);
 
         jButton1.setFont(new java.awt.Font("Berlin Sans FB Demi", 0, 14)); // NOI18N
         jButton1.setText("Reset");
         jPanel1.add(jButton1);
-        jButton1.setBounds(370, 420, 120, 30);
+        jButton1.setBounds(370, 440, 120, 30);
 
         jButton2.setFont(new java.awt.Font("Berlin Sans FB Demi", 0, 14)); // NOI18N
         jButton2.setText("Submit");
@@ -145,22 +114,55 @@ public class Payment extends javax.swing.JFrame {
             }
         });
         jPanel1.add(jButton2);
-        jButton2.setBounds(220, 420, 120, 30);
+        jButton2.setBounds(220, 440, 120, 30);
 
         jLabel6.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jLabel6.setText("Payment");
+        jLabel6.setText("Add Customer");
         jPanel1.add(jLabel6);
-        jLabel6.setBounds(270, 20, 220, 50);
+        jLabel6.setBounds(230, 20, 320, 50);
+
+        txtContactNo1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtContactNo1ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtContactNo1);
+        txtContactNo1.setBounds(220, 230, 270, 25);
+
+        txtContactNo2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtContactNo2ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtContactNo2);
+        txtContactNo2.setBounds(220, 290, 270, 25);
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Bus 1", "Bus 2", "Bus 3", "Bus 4" }));
+        jPanel1.add(jComboBox1);
+        jComboBox1.setBounds(220, 390, 270, 25);
+
+        txtContactNo3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtContactNo3ActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtContactNo3);
+        txtContactNo3.setBounds(220, 340, 270, 25);
+
+        jLabel8.setFont(new java.awt.Font("Berlin Sans FB Demi", 1, 14)); // NOI18N
+        jLabel8.setText("Bus Number         ");
+        jPanel1.add(jLabel8);
+        jLabel8.setBounds(90, 392, 120, 16);
 
         jButton3.setFont(new java.awt.Font("Berlin Sans FB", 0, 14)); // NOI18N
-        jButton3.setText("Add Page");
+        jButton3.setText("Payment Page");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
             }
         });
         jPanel1.add(jButton3);
-        jButton3.setBounds(60, 520, 110, 25);
+        jButton3.setBounds(60, 520, 140, 25);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
@@ -178,41 +180,65 @@ public class Payment extends javax.swing.JFrame {
         jPanel1.add(jPanel2);
         jPanel2.setBounds(60, 20, 530, 480);
 
-        getContentPane().add(jPanel1);
-        jPanel1.setBounds(0, 0, 780, 590);
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 780, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 780, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 590, Short.MAX_VALUE)
+            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createSequentialGroup()
+                    .addGap(0, 0, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 590, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGap(0, 0, Short.MAX_VALUE)))
+        );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
+    // TODO add your handling code here:
+
+    }//GEN-LAST:event_jButton2MouseClicked
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     private void txtIDActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtIDActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_txtIDActionPerformed
 
-    private void txtContactNoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContactNoActionPerformed
+    private void txtContactNo1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContactNo1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_txtContactNoActionPerformed
+    }//GEN-LAST:event_txtContactNo1ActionPerformed
 
-    private void txtAmountActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtAmountActionPerformed
+    private void txtContactNo2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContactNo2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_txtAmountActionPerformed
+    }//GEN-LAST:event_txtContactNo2ActionPerformed
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void txtContactNo3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContactNo3ActionPerformed
         // TODO add your handling code here:
-    
-    }//GEN-LAST:event_jButton2ActionPerformed
-
-    private void jButton2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton2MouseClicked
- chkEmpty();        // TODO add your handling code here:
-           
-    }//GEN-LAST:event_jButton2MouseClicked
+    }//GEN-LAST:event_txtContactNo3ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
-         AddCustomer frame1=new AddCustomer();
+
+
+   AddCustomer frame1=new AddCustomer();
     Payment frame2=new Payment();
     this.dispose();
-   frame2.setVisible(false);
-   frame1.setVisible(true);
+   frame1.setVisible(false);
+   frame2.setVisible(true);
+ 
     }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
@@ -232,20 +258,20 @@ public class Payment extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Payment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddCustomer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Payment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddCustomer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Payment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddCustomer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Payment.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AddCustomer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Payment().setVisible(true);
+                new AddCustomer().setVisible(true);
             }
         });
     }
@@ -254,21 +280,21 @@ public class Payment extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel8;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField txtAmount;
-    private javax.swing.JTextField txtContactNo;
-    private com.toedter.calendar.JDateChooser txtDate;
+    private javax.swing.JTextField txtContactNo1;
+    private javax.swing.JTextField txtContactNo2;
+    private javax.swing.JTextField txtContactNo3;
     private javax.swing.JTextField txtID;
     private javax.swing.JTextArea txtName;
     // End of variables declaration//GEN-END:variables
-
-  
 }
